924b4aae3f853fa536140068c88b2f1fe05740b3
karrasmil80 <zupablo3@gmail.com>
2025-02-27 19:17:31 +0100

    Configuración del Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+
+    // Plugin para serializar
+    kotlin("plugin.serialization") version "1.6.10"
 }
 
 group = "org.example"
@@ -11,6 +14,15 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // LOGGER
+    implementation("org.lighthousegames:logging:1.5.0")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
+
+    // Serializable JSON
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    // Serializable XML
+    implementation("io.github.pdvrieze.xmlutil:serialization-jvm:0.90.3")
 }
 
 tasks.test {
