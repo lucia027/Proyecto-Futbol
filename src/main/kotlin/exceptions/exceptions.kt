c17db17ae682eb0256de7ed260a82f7fc2a59fdc
Pablo Zuil <zupablo3@gmail.com>
2025-02-28 19:42:33 +0100

    Creación de la clase exceptions para mostrar excepciones personalizadas

---
+++ src/main/kotlin/exceptions/exceptions.kt
@@ -0,0 +1,9 @@
+package org.example.exceptions
+
+
+// Aquí se encontrarán las excepciones personalizadas
+sealed class exceptions(message: String) : Exception(message) {
+    class PersonalStorageException(message: String) : exceptions(message)
+    //class EntrenadorStorageException(message: String) // aún no estamos seguros de si debemos de hacer un storage para Entrenador
+    //class JugadorStorageException(message: String) // aún no estamos seguros de si debemos de hacer un storage para Jugador
+}
