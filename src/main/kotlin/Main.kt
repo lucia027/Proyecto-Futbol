bfdbd5bfaf40958732a38136d85d639fa12e26fe
Pablo Zuil <zupablo3@gmail.com>
2025-03-01 11:57:51 +0100

    Pruebas para escribir el Json

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,22 @@
 package org.example
 
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.example.Dto.PersonalDto
-import org.example.models.Personal
+import org.example.Storage.PersonalStorageJson
 import java.io.File
 
 fun main() {
 
+    // Ruta del archivo JSON
+    val file = File("data", "personal.json")
+
+    // Instanciamos la clase para leer el archivo
+
+    val personalJson = PersonalStorageJson().readFromFile(file, "json")
+    personalJson.forEach { println(Json.encodeToString(it)) }
 }
 
+/*
+    val file = File("data", "personal.xml")
+    val personalXml =
+ */
