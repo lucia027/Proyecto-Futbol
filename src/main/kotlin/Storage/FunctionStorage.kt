80660318e5e2723380063c6fb650a5ef8c617566
Pablo Zuil <zupablo3@gmail.com>
2025-03-01 12:54:20 +0100

    Elimina

--- src/main/kotlin/Storage/FunctionStorage.kt
+++
@@ -1,9 +0,0 @@
-package org.example.storage
-
-import org.example.Dto.PersonalDto
-import java.io.File
-
-interface FunctionStorage <T> {
-    fun readFromFile(file : File, format: String) : List<T>
-    fun writeToFile (file : File, format : String,  personal : List<T>)
-}
