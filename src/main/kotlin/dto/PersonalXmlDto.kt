23bb9e551e6e51fdd01330f6b5c00c9e2fcf4dff
Pablo Zuil <zupablo3@gmail.com>
2025-03-04 00:06:19 +0100

    Creacion de la data class EtiquetaEquipo para que se pueda leer el xml debido a la primera etiqueta de apertura, tambien he creado PersonalDtoXml para que se pueda deserializar

--- src/main/kotlin/dto/PersonalXmlDto.kt
+++ src/main/kotlin/dto/PersonalXmlDto.kt
@@ -1,39 +1,55 @@
-package org.example
+package org.example.dto
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-
+import nl.adaptivity.xmlutil.serialization.XmlElement
 
 @Serializable
-data class PersonalDto(
+data class PersonalXmlDto(
     @SerialName("id")
     var id: Long,
+    @SerialName("tipo")
+    val tipo: String,
+    @XmlElement
     @SerialName("nombre")
     var nombre: String,
+    @XmlElement
     @SerialName("apellidos")
     var apellidos: String,
-    @SerialName("fecha_nacimiento")
+    @XmlElement
+    @SerialName("fechaNacimiento")
     var fecha_nacimiento: String,
-    @SerialName("fecha_incorporacion")
+    @XmlElement
+    @SerialName("fechaIncorporacion")
     var fecha_incorporacion: String,
+    @XmlElement
     @SerialName("salario")
     var salario: Double,
+    @XmlElement
     @SerialName("pais")
     var pais: String,
+    @XmlElement
     @SerialName("rol")
     val rol: String,
+    @XmlElement
     @SerialName("especialidad")
     val especialidad: String?,
+    @XmlElement
     @SerialName("posicion")
     var posicion: String?,
+    @XmlElement
     @SerialName("dorsal")
     var dorsal: Int?,
+    @XmlElement
     @SerialName("altura")
     var altura: Double?,
+    @XmlElement
     @SerialName("peso")
     var peso: Double?,
+    @XmlElement
     @SerialName("goles")
     var goles: Int?,
-    @SerialName("partidos_jugados")
+    @XmlElement
+    @SerialName("partidosJugados")
     var partidos_jugados: Int?
 ) : java.io.Serializable
