2b8018a18d95e153742f7f9c91f129a11bf5cefd
Pablo Zuil <zupablo3@gmail.com>
2025-03-03 13:37:03 +0100

    Añadido de @Serializable y @SerialName, arreglo del main

--- src/main/kotlin/dto/EntrenadorDto.kt
+++ src/main/kotlin/dto/EntrenadorDto.kt
@@ -1,17 +1,28 @@
 
 package org.example.Dto
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import org.example.models.Entrenador.Especializacion
 import org.example.models.Personal.Companion.NEW_ID
 
+@Serializable
 class EntrenadorDto (
+    @SerialName("id")
     val id: Int,
+    @SerialName("nombre")
     val nombre: String,
+    @SerialName("apellidos")
     val apellidos: String,
+    @SerialName("fecha_nacimiento")
     val fechaNacimiento: String,
+    @SerialName("fecha_incorporacion")
     val fechaIncorporacion: String,
+    @SerialName("salario")
     val salario: Double,
+    @SerialName("pais")
     val pais: String,
+    @SerialName("especialidad")
     val especialidad: String,
 ){
 
--- src/main/kotlin/dto/JugadorDto.kt
+++ src/main/kotlin/dto/JugadorDto.kt
@@ -1,22 +1,36 @@
 
 package org.example.Dto
 
+import kotlinx.serialization.SerialName
 import org.example.models.Jugador.Posicion
 import org.example.models.Personal.Companion.NEW_ID
 
 class JugadorDto (
+    @SerialName("id")
     val id: Int,
+    @SerialName("nombre")
     val nombre: String,
+    @SerialName("apellidos")
     val apellidos: String,
+    @SerialName("fecha_nacimiento")
     val fechaNacimiento: String,
+    @SerialName("fecha_incorporacion")
     val fechaIncorporacion: String,
+    @SerialName("salario")
     val salario: Double,
+    @SerialName("pais")
     val pais: String,
+    @SerialName("posicion")
     val posicion: String,
+    @SerialName("dorsal")
     val dorsal: Int,
+    @SerialName("altura")
     val altura: Double,
+    @SerialName("peso")
     val peso: Double,
+    @SerialName("goles")
     val goles: Int,
+    @SerialName("partidos_jugados")
     val partidosJugados: Int
 ){
 
