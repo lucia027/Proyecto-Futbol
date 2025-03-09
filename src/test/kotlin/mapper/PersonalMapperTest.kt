1de5e27c4c8274a92d9f8ea6ce671c0a91628154
Luuu027 <lucia.fuertes.06@gmail.com>
2025-03-09 01:13:40 +0100

    Añadido de configuracion y eliminacion de archivos sobrantes.

--- src/test/kotlin/mapper/PersonalMapperTest.kt
+++ src/test/kotlin/mapper/PersonalMapperTest.kt
@@ -1,4 +1,14 @@
 package mapper
 
+import org.example.models.Personal
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class PersonalMapperTest {
+
+    @Test
+    @DisplayName("Test Producto to Dto")
+    fun personalToPersonalDto(){
+
+    }
 }
